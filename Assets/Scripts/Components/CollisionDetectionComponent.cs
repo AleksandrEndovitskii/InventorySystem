fbb8c03cd9a6cf65b8491eb548b2f019b95d60be
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-17 20:59:24 +0300

    Add collision detection to backpack

---
+++ Assets/Scripts/Components/CollisionDetectionComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    public class CollisionDetectionComponent : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            Debug.Log($"{this.gameObject.name} invoked OnCollisionEnter with {other.gameObject.name}");
+        }
+        private void OnCollisionExit(Collision other)
+        {
+            Debug.Log($"{this.gameObject.name} invoked OnCollisionExit with {other.gameObject.name}");
+        }
+    }
+}
