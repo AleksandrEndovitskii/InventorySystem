b1209701e6365502150246ea9784e78090febb52
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-17 17:20:08 +0300

    Add 3 inventory items

---
+++ Assets/Scripts/Models/InventoryItems/InventoryItemModel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Models.InventoryItems
+{
+    public class InventoryItemModel : MonoBehaviour
+    {
+        public int Id;
+        public string Name;
+        public string Type;
+        public float Weight;
+    }
+}
