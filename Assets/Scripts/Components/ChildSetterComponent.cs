cdd291de902e8d19f2dbd707029ee66ae6590faf
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-18 18:17:09 +0300

    Add partial implementation of setting inventory items to backpack slots

---
+++ Assets/Scripts/Components/ChildSetterComponent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models.InventoryItems;
+using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(CollisionDetectionComponent))]
+    public class ChildSetterComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private List<TypeTransform> _typeTransforms = new List<TypeTransform>();
+
+        [Serializable]
+        public class TypeTransform
+        {
+            public string Type;
+            public Transform Transform;
+        }
+
+        private CollisionDetectionComponent _collisionDetectionComponent;
+
+        private void Awake()
+        {
+            _collisionDetectionComponent = this.gameObject.GetComponent<CollisionDetectionComponent>();
+        }
+        private void Start()
+        {
+            _collisionDetectionComponent.CollisionEnter += OnCollisionEnter;
+            _collisionDetectionComponent.CollisionExit += OnCollisionExit;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var inventoryItemModel = collision.gameObject.GetComponent<InventoryItemModel>();
+            if (inventoryItemModel == null)
+            {
+                return;
+            }
+
+            var typeTransform = _typeTransforms.FirstOrDefault(x=>x.Type == inventoryItemModel.Type);
+            if (typeTransform == null)
+            {
+                return;
+            }
+
+            Destroy(collision.gameObject);
+            // collision.gameObject.transform.parent = typeTransform.Transform;
+            // collision.gameObject.transform.localPosition = Vector3.zero;
+        }
+        private void OnCollisionExit(Collision obj)
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Components/CollisionDetectionComponent.cs
+++ Assets/Scripts/Components/CollisionDetectionComponent.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Components
 {
     public class CollisionDetectionComponent : MonoBehaviour
     {
+        public Action<Collision> CollisionEnter = delegate { };
+        public Action<Collision> CollisionExit = delegate { };
+
         private void OnCollisionEnter(Collision other)
         {
             Debug.Log($"{this.gameObject.name} invoked OnCollisionEnter with {other.gameObject.name}");
