fbf175964c7c0d961a692fd303484cf84ee02f71
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-18 22:53:42 +0300

    Add loading of inventory items on start of application

--- Assets/Scripts/Models/BackpackModel.cs
+++ Assets/Scripts/Models/BackpackModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Models.InventoryItems;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Models
@@ -8,9 +9,28 @@ namespace Models
     {
         private List<InventoryItemModel> _inventoryItemModels = new List<InventoryItemModel>();
 
+        private const string _inventoryItemModelsKey = "InventoryItemModels";
+
+        private void Awake()
+        {
+            Load();
+        }
+
         public void AddItem(InventoryItemModel inventoryItemModel)
         {
             _inventoryItemModels.Add(inventoryItemModel);
+        }
+
+        private void Load()
+        {
+            Debug.Log($"Loading of key({_inventoryItemModelsKey}) started");
+
+            var inventoryItemModels = new List<InventoryItemModel>();
+            var inventoryItemModelsDefaultValue = JsonConvert.SerializeObject(inventoryItemModels);
+            var jsonString = PlayerPrefs.GetString(_inventoryItemModelsKey, inventoryItemModelsDefaultValue);
+            _inventoryItemModels = JsonConvert.DeserializeObject<List<InventoryItemModel>>(jsonString);
+
+            Debug.Log($"Loading of key({_inventoryItemModelsKey}) with value({jsonString}) finished");
         }
     }
 }
