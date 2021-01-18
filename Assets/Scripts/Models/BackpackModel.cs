baa4439dff31daf75b6e18d74da283be34c24166
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-18 18:38:37 +0300

    Add backpack model

---
+++ Assets/Scripts/Models/BackpackModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Models.InventoryItems;
+using UnityEngine;
+
+namespace Models
+{
+    public class BackpackModel : MonoBehaviour
+    {
+        private List<InventoryItemModel> _inventoryItemModels = new List<InventoryItemModel>();
+
+        public void AddItem(InventoryItemModel inventoryItemModel)
+        {
+            _inventoryItemModels.Add(inventoryItemModel);
+        }
+    }
+}
