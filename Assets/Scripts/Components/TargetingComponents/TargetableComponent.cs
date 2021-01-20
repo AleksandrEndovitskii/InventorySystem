124e6501e07bce849b95cde95f2360af7ed42795
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-20 21:34:55 +0300

    Add targeting manager

---
+++ Assets/Scripts/Components/TargetingComponents/TargetableComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Components.TargetingComponents
+{
+    public class TargetableComponent : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Managers/TargetingManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Components.TargetingComponents;
+using UnityEngine;
+
+namespace Managers
+{
+    public class TargetingManager : MonoBehaviour
+    {
+        public Action<TargetableComponent> TargetedObjectChanged = delegate {  };
+
+        [SerializeField]
+        private float _selectionRange = 1000f;
+
+        public TargetableComponent TargetedObject
+        {
+            get
+            {
+                return _targetedObject;
+            }
+            set
+            {
+                if (_targetedObject == value)
+                {
+                    return;
+                }
+
+                var previouslySelectedObjectName = "None";
+                if (_targetedObject != null &&
+                    _targetedObject.gameObject != null)
+                {
+                    previouslySelectedObjectName = _targetedObject.gameObject.name;
+                }
+
+                var currentlySelectedObjectName = "None";
+                if (value != null &&
+                    value.gameObject != null)
+                {
+                    currentlySelectedObjectName = value.gameObject.name;
+                }
+
+                Debug.Log(
+                    $"Targeted object changed from {previouslySelectedObjectName} to {currentlySelectedObjectName}");
+
+                _targetedObject = value;
+
+                TargetedObjectChanged.Invoke(_targetedObject);
+            }
+        }
+
+        private TargetableComponent _targetedObject;
+
+        private void Update()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // dont hit anything - nothing to do here
+            if (!Physics.Raycast(ray, out var hit))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * _selectionRange, Color.red);
+                TargetedObject = null;
+
+                return;
+            }
+
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
+            TargetedObject = hit.transform.gameObject.GetComponent<TargetableComponent>();
+        }
+    }
+}
