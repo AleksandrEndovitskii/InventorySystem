c28b122c0704bb2bf0e4338059b2a2395e9e943e
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2021-01-20 15:42:09 +0300

    Add backpack canvas to backpack

---
+++ Assets/Scripts/Components/MainCameraCanvasWorldCameraSetterComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(Canvas))]
+    public class MainCameraCanvasWorldCameraSetterComponent : MonoBehaviour
+    {
+        private Canvas _canvas;
+
+        private void Awake()
+        {
+            _canvas = this.gameObject.GetComponent<Canvas>();
+        }
+        private void Start()
+        {
+            _canvas.worldCamera = Camera.main;
+        }
+    }
+}
